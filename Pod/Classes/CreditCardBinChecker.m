1140401f9712699cc005bcc77c061238f591dedc
Abel Pascual <abelpascual@gmail.com>
2018-07-18 16:42:52 +0200

    Normalize card regex

--- Pod/Classes/CreditCardBinChecker.m
+++ Pod/Classes/CreditCardBinChecker.m
@@ -14,6 +14,7 @@
 
 @implementation CreditCardBinChecker
 
+// 20180718 Updated using: https://github.com/braintree/credit-card-type/blob/master/index.js
 +(CreditCardBinChecker*)shareManager{
     static CreditCardBinChecker *shareManager = nil;
     static dispatch_once_t onceToken;
@@ -22,46 +23,66 @@
         shareManager.binRules = @[
                                   @{
                                       @"issuer": @"visa",
-                                      @"initNumber": @[@4],
                                       @"maxNumbers": @[@16, @18, @19],
                                       @"maxSecurityCardCode": @3,
-                                      @"regex": @"^4"
+                                      @"regex": @"^4\\d*$",
+                                      @"gaps": @[@4, @8, @12],
                                       },
                                   @{
                                       @"issuer": @"mastercard",
-                                      @"initNumber": @[@5],
                                       @"maxNumbers": @[@16],
                                       @"maxSecurityCardCode": @3,
-                                      @"regex": @"^5[1-5]"
-                                  },
+                                      @"regex": @"^(5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[0-1]|2720)\\d*$",
+                                      @"gaps": @[@4, @8, @12],
+                                      },
                                   @{
                                       @"issuer": @"amex",
-                                      @"initNumber": @[@34,@37],
                                       @"maxNumbers": @[@15],
                                       @"maxSecurityCardCode": @4,
-                                      @"regex": @"^3[47]"
-                                  },
-                                  @{
-                                      @"issuer": @"maestro",
-                                      @"initNumber": @[@34,@37],
-                                      @"maxNumbers": @[@12, @13, @14, @15, @16, @17, @18, @19],
-                                      @"maxSecurityCardCode": @3,
-                                      @"regex": @"^5[06-9]"
+                                      @"regex": @"^3[47]\\d*$",
+                                      @"gaps": @[@4, @10],
                                       },
                                   @{
                                       @"issuer": @"diners",
-                                      @"initNumber": @[@301,@302,@303,@304,@305,@309,@36,@38,@39],
-                                      @"maxNumbers": @[@14,@16],
+                                      @"maxNumbers": @[@14, @16, @19],
                                       @"maxSecurityCardCode": @3,
-                                      @"regex": @"^3(?:0[0-59]|[689])"
-                                  },
+                                      @"regex": @"^3(0[0-5]|[689])\\d*$",
+                                      @"gaps": @[@4, @10],
+                                      },
                                   @{
-                                      @"issuer": @"hiper",
-                                      @"initNumber": @[@38,@60],
-                                      @"maxNumbers": @[@12,@16,@19],
+                                      @"issuer": @"discover",
+                                      @"maxNumbers": @[@16, @19],
                                       @"maxSecurityCardCode": @3,
-                                      @"regex" : @"^(606282|637095|3841[046]0)"
-                                  }
+                                      @"regex": @"^(6011|65|64[4-9])\\d*",
+                                      @"gaps": @[@4, @8, @12],
+                                      },
+                                  @{
+                                      @"issuer": @"jcb",
+                                      @"maxNumbers": @[@16, @17, @18, @19],
+                                      @"maxSecurityCardCode": @3,
+                                      @"regex": @"^(2131|1800|35)\\d*$",
+                                      @"gaps": @[@4, @8, @12],
+                                      },
+                                  @{
+                                      @"issuer": @"unionpay",
+                                      @"maxNumbers": @[@16, @17, @18, @19],
+                                      @"maxSecurityCardCode": @3,
+                                      @"regex": @"^(((620|(621(?!83|88|98|99))|622(?!06|018)|62[3-6]|627[02,06,07]|628(?!0|1)|629[1,2]))\\d*|622018\\d{12})$",
+                                      @"gaps": @[@4, @8, @12],
+                                      },
+                                  @{
+                                      @"issuer": @"maestro",
+                                      @"maxNumbers": @[@12, @13, @14, @15, @16, @17, @18, @19],
+                                      @"maxSecurityCardCode": @3,
+                                      @"regex": @"^(5[06-9]|6[37])\\d*$"
+                                      },
+                                  
+                                  @{
+                                      @"issuer": @"mir",
+                                      @"maxNumbers": @[@16, @17, @18, @19],
+                                      @"maxSecurityCardCode": @3,
+                                      @"regex" : @"^(220[0-4])\\d*$"
+                                      }
                                   ];
         
     });
@@ -73,19 +94,19 @@
         CardBinRule *rule = [CardBinRule cardBinRule:dc];
         if([[self shareManager] matchCardNumber:cardNumber
                                     withPattern:rule.regex])
-            return rule;
+        return rule;
     }
     return nil;
 }
 
--(BOOL)matchCardNumber:(NSString*)cardNumber withPattern:(NSString*)pattern{
-    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern: pattern
-                                                                           options:0
-                                                                             error:nil];
-    NSArray *matches = [regex matchesInString:cardNumber
-                                      options:0
-                                        range:NSMakeRange(0, [cardNumber length])];
-    return (matches != nil && matches.count > 0);
+-(BOOL)matchCardNumber:(NSString*)cardNumber withPattern:(NSString*)pattern {
+    NSPredicate *myTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", pattern];
+    
+    if([myTest evaluateWithObject:cardNumber]) {
+        return YES;
+    }
+    
+    return NO;
 }
 
 +(BOOL)isValidCard:(NSString*)cardNumber{
@@ -94,7 +115,7 @@
     if (rule && [CreditCardBinChecker checkNumber:cardNumber]) {
         for (NSNumber *maxNumber in rule.maxNumbers) {
             if ([maxNumber intValue] == cardNumber.length)
-                return YES;
+            return YES;
         }
     }
     return NO;
@@ -107,10 +128,10 @@
         int digitInt = [[NSString stringWithFormat:@"%c", [cardNumber characterAtIndex:i]] intValue];
         
         if ((odd = !odd))
-            digitInt *= 2;
+        digitInt *= 2;
         
         if (digitInt > 9)
-            digitInt -= 9;
+        digitInt -= 9;
         
         sum += digitInt;
     }
